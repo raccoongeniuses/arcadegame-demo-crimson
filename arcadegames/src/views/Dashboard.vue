b5ab0989813412faf048604346c237552c51c488
hiromihanson <hiromihanson@crimson.network>
2025-02-24 15:48:01 +0700

    feat: Remove Mortal Kombat data and make the status to be in green and red highlight

--- arcadegames/src/views/Dashboard.vue
+++ arcadegames/src/views/Dashboard.vue
@@ -11,18 +11,19 @@
         <table class="table min-w-full border border-gray-700">
           <thead>
             <tr class="bg-gray-800 text-white">
-              <th class="p-3 text-center">No.</th>
-              <th class="p-3 text-center">Game Name</th>
-              <th class="p-3 text-center">Status</th>
-              <th class="p-3 text-center">Launch</th>
+              <th class="p-3 text-center font-bold">No.</th>
+              <th class="p-3 text-center font-bold">Game Name</th>
+              <th class="p-3 text-center font-bold">Status</th>
+              <th class="p-3 text-center font-bold">Launch</th>
             </tr>
           </thead>
           <tbody>
             <tr v-for="(game, index) in games" :key="index" class="border-b border-gray-700">
-              <td class="p-3 text-center">{{ index + 1 }}.</td>
-              <td class="p-3 text-center">{{ game.name }}</td>
+              <td class="p-3 text-center font-bold">{{ index + 1 }}.</td>
+              <td class="p-3 text-center font-bold text-white">{{ game.name }}</td>
               <td class="p-3 text-center">
-                <span class="badge" :class="game.status === 'Online' ? 'badge-success' : 'badge-error'">
+                <span class="px-3 py-1 rounded font-semibold text-white"
+                      :class="game.status === 'Online' ? 'bg-green-500' : 'bg-red-500'">
                   {{ game.status }}
                 </span>
               </td>
@@ -46,7 +47,6 @@ export default {
       games: [
         { name: "Street Fighter", status: "Online", image: "/sf6.png", link: "http://sf.arcadegame-stream.com/?currency=THB&lang=en&params=ZQHxo6Nil4BIJ9UTXArXxOepNjPxBbWo2iscmE5l586ilh4p6CHTAwRj7Ikf%0A6svkV7j16RpTceT%2F%2F4pA6A4dYGK2EAqH2RRpGKZy7wkApxFlGg64mgA7dLLS%0Ay43%2Bbc%2FoUsgC5QlQmtEJr%2BKODQC3Ixt%2FX54wuElFBcDctztqcNvMUxxp9B91%0Ar2oRzbHCr1o4SsjfSghPQBqA6yO90fqjukanFdqplgylp%2Fh6dwbghFrpIxw6%0ASN9cMbM3vcs9Gp9xid6wjyVO3CJmDY1gznZ13MYdqSNhjpXmhFl0%2FkHSMgVx%0A84D%2BF%2FDnUea%2BYqcOWYGhG1dZ9heQ6zs%3D%0A&returnURL=https%3A%2F%2Fm13.ns86.kingdomhall729.com%2Fportal&test=0" },
         { name: "Tekken 8", status: "Offline", image: "/tekken8.png", link: "#" },
-        { name: "Mortal Kombat", status: "Offline", image: "/mortalkombat.png", link: "#" },
       ],
     };
   },
